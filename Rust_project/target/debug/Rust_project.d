cb337599a16b47096a3c801f98e951e7d60f5eb4
Fernando Ivan Salas <fer.ivan.gatto96@gmail.com>
2025-10-01 22:47:58 -0600

    Add files via upload

---
+++ Rust_project/target/debug/Rust_project.d
@@ -0,0 +1,1 @@
+C:\Users\user\Desktop\Rust\Rust_project\target\debug\Rust_project.exe: C:\Users\user\Desktop\Rust\Rust_project\src\main.rs
---
+++ Rust_project/target/debug/deps/Rust_project.d
@@ -0,0 +1,5 @@
+C:\Users\user\Desktop\Rust\Rust_project\target\debug\deps\Rust_project.d: src\main.rs
+
+C:\Users\user\Desktop\Rust\Rust_project\target\debug\deps\Rust_project.exe: src\main.rs
+
+src\main.rs:
